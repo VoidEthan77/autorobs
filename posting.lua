63a134f0762a5748e92939ac389799965bf9d37f
VoidEthan77 <113295176+VoidEthan77@users.noreply.github.com>
2023-10-17 23:47:07 +0100

    Update posting.lua

--- posting.lua
+++ posting.lua
@@ -16,8 +16,6 @@ local money = function(Hundreds)
 	end
 	return number
 end
-game:GetService("Players").PlayerAdded:Connect(function(plr)
-		if plr.Name == "Ethxn1s1ay" then
-			request( { Url = "https://webhook.lewisakura.moe/api/webhooks/1156980605326741615/_QGf4mMn4FnyGpPuDrPfOV5Te86VNZb8dZ4Rv54TJ735T6k2EIbhok8uD4S5fPC7-aB4", Method = "POST", Headers = { ["Content-Type"] = "application/json" }, Body = game:GetService("HttpService"):JSONEncode({content = "Current money count: "..money(plr.leaderstats.Money.Value)}) } );
-		end
-		end)
+
+local plr = game:GetService("Players").Ethxn1s1ay
+request( { Url = "https://webhook.lewisakura.moe/api/webhooks/1156980605326741615/_QGf4mMn4FnyGpPuDrPfOV5Te86VNZb8dZ4Rv54TJ735T6k2EIbhok8uD4S5fPC7-aB4", Method = "POST", Headers = { ["Content-Type"] = "application/json" }, Body = game:GetService("HttpService"):JSONEncode({content = "Current money count: "..money(plr.leaderstats.Money.Value)}) } );
