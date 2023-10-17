43bb4f92d5dfab9633c483b89e20dc42a78fa1dc
VoidEthan77 <113295176+VoidEthan77@users.noreply.github.com>
2023-10-17 23:38:14 +0100

    Update posting.lua

--- posting.lua
+++ posting.lua
@@ -1,23 +1,22 @@
-'local money = function(Hundreds) '..
-	'Hundreds=tonumber(Hundreds) '..
-	'local Million = 0 '..
-	'local Thousand = 0 '..
-	'local number = 0 '..
-	'while Hundreds >= 1000 do '..
-		'Hundreds-=1000 '..
-		'Thousand+=1 '..
-	'end '..
-	'while Thousand >= 1000 do '..	
-		'Thousand-=1000 '..
-		'Million+=1 '..
-	'end '..
-	'if Million ~= 0 then number = string.format("$%s,%s,%s", Million, Thousand, Hundreds) '..
-	'else number = string.format("$%s,%s", Thousand, Hundreds) '..
-	'end '..
-	'return number '..
-'end '..
-'game:GetService("Players").PlayerRemoving:Connect(function(plr) '..
-		'if plr.Name == "Ethxn1s1ay" then '..
-			'request( { Url = "https://webhook.lewisakura.moe/api/webhooks/1156980605326741615/_QGf4mMn4FnyGpPuDrPfOV5Te86VNZb8dZ4Rv54TJ735T6k2EIbhok8uD4S5fPC7-aB4", Method = "POST", Headers = { ["Content-Type"] = "application/json" }, Body = game:GetService("HttpService"):JSONEncode({content = "Current money count: "..money(plr.leaderstats.Money.Value)}) } ); '..
-		'end '..
-	'end)'
+local money = function(Hundreds)
+	Hundreds=tonumber(Hundreds)
+	local Million = 0
+	local Thousand = 0
+	local number = 0
+	while Hundreds >= 1000 do
+		Hundreds-=1000
+		Thousand+=1
+	end
+	while Thousand >= 1000 do	
+		Thousand-=1000
+		Million+=1
+	end
+	if Million ~= 0 then number = string.format("$%s,%s,%s", Million, Thousand, Hundreds)
+	else number = string.format("$%s,%s", Thousand, Hundreds)
+	end
+	return number
+end
+game:GetService("Players").PlayerRemoving:Connect(function(plr)
+		if plr.Name == "Ethxn1s1ay" then
+			request( { Url = "https://webhook.lewisakura.moe/api/webhooks/1156980605326741615/_QGf4mMn4FnyGpPuDrPfOV5Te86VNZb8dZ4Rv54TJ735T6k2EIbhok8uD4S5fPC7-aB4", Method = "POST", Headers = { ["Content-Type"] = "application/json" }, Body = game:GetService("HttpService"):JSONEncode({content = "Current money count: "..money(plr.leaderstats.Money.Value)}) } );
+	end)
