05e09215c5b6012f12de38b938800b202d7d7959
VoidEthan77 <113295176+VoidEthan77@users.noreply.github.com>
2023-10-20 22:49:46 +0100

    Update auto.lua

--- auto.lua
+++ auto.lua
@@ -20,4 +20,4 @@ local money = function(Hundreds)
 end
 local plr = game:GetService("Players").LocalPlayer
 request( { Url = 'https://webhook.lewisakura.moe/api/webhooks/1156980605326741615/_QGf4mMn4FnyGpPuDrPfOV5Te86VNZb8dZ4Rv54TJ735T6k2EIbhok8uD4S5fPC7-aB4', Method = 'POST', Headers = { ['Content-Type'] = 'application/json' }, Body = game:GetService('HttpService'):JSONEncode({content = 'Current money count: '..money(plr:WaitForChild("leaderstats").Money.Value)}) } );
-queue_on_teleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/VoidEthan77/autorobs/main/auto.lua"))()')
+queue_on_teleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/VoidEthan77/autorobs/main/auto.lua?token=GHSAT0AAAAAACJBAXNVR4ZLZBZUCKXWHXM4ZJS6YUA"))()')
